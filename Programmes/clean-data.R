f87aaa91ec54169da2a2c40e7775c1c7a143536e
Samueljvs <59219545+Samueljvs@users.noreply.github.com>
2022-03-28 10:31:15 +1300

    Working on setup

---
+++ Programmes/clean-data.R
@@ -0,0 +1,12 @@
+##    Programme:  Clean StatsNZ data
+##
+##    Objective:  This programme will clean raw data download of NZ-country stock of direct investment from StatsNZ
+##
+##    Plan of  :  
+##    Attack   : 
+##
+##    Author   :  Sam Verevis, 3/28/2022
+##
+    ## Load raw data
+    ##
+        raw_df <- read_csv("Data_Raw/IIP456201_20220328_101307_67.csv")  
